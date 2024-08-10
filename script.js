bc593d44148226ecbd0a47c0ef6b295c148b5fbe
tammy20240509 <2023dlsxedgeishizuka@gmail.com>
2024-08-10 20:16:38 +0900

    資源のデプロイ

--- script.js
+++ script.js
@@ -42,6 +42,27 @@ const app = new Vue({
       
       //結果リストを表示用配列に代入
       this.dataList = response.data.List;
-    },
+      },
+      //以下DELETE用。IDの入力チェック（空白か数字以外なら終了）
+      addDelete: async function () {
+
+          //IDの入力チェック（空白か数字以外なら終了）
+          if (!this.ID || isNaN(this.ID)) {
+              console.log("IDに数値が入力されていません");
+              return;
+          }
+
+          //POSTメソッドで送るパラメーターを作成
+          const param = {
+              ID: this.ID,
+          };
+
+          //DELETE用のAPIを呼び出し
+          const response = await axios.post('https://m3h-ishizuka-functionapi.azurewebsites.net/api/DELETE', param);
+
+          //結果をコンソールに出力
+          console.log(response.data);
+
+      },
   },
 });
